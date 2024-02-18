c35231b3897edaab41b031c60cd519b316c204ba
Abdulkadir <qansax30@hotmail.com>
2024-02-18 21:24:45 +0100

    Push to github

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/1.png.webp";
+import product2 from "./assets/2.png.webp";
+import product3 from "./assets/3.png.webp";
+import product4 from "./assets/4.png.png";
+import product5 from "./assets/5.png.png";
+import product6 from "./assets/6.png.png";
+import product7 from "./assets/7.png.png";
+import product8 from "./assets/8.png.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Nike",
+    price: 130.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Nike)",
+    price: 170.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Nike",
+    price: 250.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Nike",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Nike",
+    price: 189.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Nike",
+    price: 210.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Nike",
+    price: 150.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Nike",
+    price: 240.0,
+    productImage: product8,
+  },
+];
